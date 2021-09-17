e1ee622ed21455017ca8b3fed136581e8f526a58
Luffier <Luffier@users.noreply.github.com>
2021-09-17 21:56:36 +0100

    Create meta file

---
+++ meta/the-pirate-calendar.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name         The Pirate Calendar (for trakt.tv)
+// @version      0.4.1
+// @description  Adds RARBG links to trakt.tv (now with settings menu!)
+// @author       luffier
+// @namespace    PirateCalendar
+// @license      MIT
+// ==/UserScript==
